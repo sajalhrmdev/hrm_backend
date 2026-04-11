7addd663cb2d7c7c803542cbbf50a805ab3304da
DebaprasadM <debaprasadmaiti2000@gmail.com>
2026-04-11 19:02:18 +0530

    cors added

--- server.ts
+++ server.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
 import deparmentRoutes from "./routes/department.routes.js";
 import designationRoutes from "./routes/designation.routes.js";
-
+import cors from "cors";
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:3001", // frontend URL
+    credentials: true,
+  })
+);
 const port = 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
