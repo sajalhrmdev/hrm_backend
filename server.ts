dc32dafca85a557249fc07a970f7e2bfa4341791
sajalhrmdev <sajalhrmdev@gmail.com>
2026-04-22 18:50:40 +0530

    port added

--- server.ts
+++ server.ts
@@ -25,7 +25,7 @@ app.use(
     credentials: true,
   }),
 );
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
