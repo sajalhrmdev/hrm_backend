4addb692e6c97047a59d6df8108a5d8410eef44e
DebaprasadM <debaprasadmaiti2000@gmail.com>
2026-07-07 14:03:33 +0530

    employeeroutecommentout

--- routes/employee.routes.ts
+++ routes/employee.routes.ts
@@ -1,23 +1,23 @@
-// routes/employee.routes.ts
+// // routes/employee.routes.ts
 
-import express from "express";
-import {
-  // createEmployee,
-  getAllEmployees,
-  getEmployeeById,
-  updateEmployee,
-  deleteEmployee,
-  bulkCreateEmployees,
-  createEmployeeWithUser,
-} from "../controllers/employee.controller.js";
+// import express from "express";
+// import {
+//   // createEmployee,
+//   getAllEmployees,
+//   getEmployeeById,
+//   updateEmployee,
+//   deleteEmployee,
+//   bulkCreateEmployees,
+//   createEmployeeWithUser,
+// } from "../controllers/employee.controller.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/", createEmployeeWithUser);
-router.post("/bulkCreate", bulkCreateEmployees);
-router.get("/all", getAllEmployees);
-router.get("/:id", getEmployeeById);
-router.put("/:id", updateEmployee);
-router.delete("/:id", deleteEmployee);
+// router.post("/", createEmployeeWithUser);
+// router.post("/bulkCreate", bulkCreateEmployees);
+// router.get("/all", getAllEmployees);
+// router.get("/:id", getEmployeeById);
+// router.put("/:id", updateEmployee);
+// router.delete("/:id", deleteEmployee);
 
-export default router;
+// export default router;
